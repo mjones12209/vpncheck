87b514b49860a156a4be4041dc2df8567f5ff600
m.jones12209 <m.jones12209@gmail.com>
2020-11-19 10:45:20 -0500

    added killing of process transmission & kodi on openvpn error

--- vpncheck.sh
+++ vpncheck.sh
@@ -6,6 +6,14 @@ systemctl status openvpn-client@nyc > /tmp/vpncheck
 #location of the vpncheck files
 #$vpnCheck="/tmp/vpncheck"
 
+        #check for errors and if so notify user and kill transmission
+	if ( $( grep -qi "error" "/tmp/vpncheck" ) ); then
+		zenity --error --height=100 --width=200 --text="<span>OpenVPN Service has Errors, Transmission killed</span>" --title="Systemctl - Error" --ok-label="Ok"
+		pkill -9 $(pidof transmission-gtk)
+		systemctl stop transmission-daemon
+		systemctl stop transmission
+	fi
+
 
         #if grep finds any of these phrases in log files restart vpn
         if ( $( grep -qi "sigusr1" "/tmp/vpncheck" ) || $( grep -qi "tlS error" "/tmp/vpncheck") ); then
@@ -17,9 +25,10 @@ systemctl status openvpn-client@nyc > /tmp/vpncheck
 
         fi    
 
+
 ping -c 4 4.2.2.2 >> /tmp/vpncheck
 
-        if ( $( grep -qi "100%" "/tmp/vpncheck") ); then
+        if ( $( grep -qi "100% packet loss" "/tmp/vpncheck") ); then
 
             systemctl restart openvpn-client@nyc
 
